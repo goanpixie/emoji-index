3fd9e4455b494c110b5368bde54461a6e055f639
Priyanka <priyanka3007@gmail.com>
2019-10-29 16:36:49 -0700

    added ListItem

---
+++ src/components/ListItem/styles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Item, Title, Description } from './styles';
+
+const ListItem = () => {
+	return (
+		<Item>
+			<Title></Title>
+			<Description/>
+		</Item>
+	)
+}
+
+export default ListItem;
