960887c700eb23a8d5469c2f3c3f253c78fb568f
Priyanka <priyanka3007@gmail.com>
2019-11-01 16:28:48 -0700

    very close to get emodex

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Item, Title, Description } from './styles';
-//emoji datasource
+import { Item, Title, Description} from './styles';
+import Emoji from 'a11y-react-emoji';
+import Emodex from 'emoji-datasource';
+
+
 const ListItem = () => {
 	return (
 		<Item>
-			<Title>:)</Title>
-			<Description>Smiley</Description>
+			{Emodex.map(emoji=>(
+				<React.Fragment>
+					<Title><span aria-label={emoji.short_name}></span></Title>
+					<Description><Emoji role="img">{"&#x"}{emoji.unified}</Emoji></Description>
+				</React.Fragment>
+			))}
+
+
+			<span aria-hidden="true" role="img">🤫</span>
+
 		</Item>
 	)
 }
